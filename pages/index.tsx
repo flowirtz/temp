207bc5bbcfdd459dfb60a14062e9e87ba2694cd9
flowirtz <6052785+flowirtz@users.noreply.github.com>
2021-07-31 22:05:35 +0100

    Add frontend for airports search

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,15 @@
 import { NextPage } from "next";
+import AirportsSearch from "../components/airportsSearch";
 
 import Layout from "../components/layout";
-import useApiData from "../hooks/use-api-data";
-import Airport from "../types/airport";
 
 const Page: NextPage = () => {
-  const airports = useApiData<Airport[]>("/api/airports", []);
 
   return (
     <Layout>
-      <h1 className="text-2xl">Code Challenge: Airports</h1>
+      <h1 className="text-2xl mb-8">Code Challenge: Airports</h1>
 
-      <h2 className="mt-10 text-xl">All Airports</h2>
-
-      <div>
-        {airports.map((airport) => (
-          <a
-            href={`/airports/${airport.iata.toLowerCase()}`}
-            key={airport.iata}
-            className="mt-5 flex items-center shadow p-5 border"
-          >
-            <div>
-              {airport.name}, {airport.city}
-            </div>
-            <div className="ml-auto text-mono">{airport.country}</div>
-          </a>
-        ))}
-      </div>
+      <AirportsSearch />
     </Layout>
   );
 };
